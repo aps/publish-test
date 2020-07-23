b093c5aee280a6b1527485684f0f454a464e38ce
Arne Poths <mail@arnepoths.de>
2020-07-23 12:39:49 +0200

    publish test

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("kotlin-android")
     id("kotlin-android-extensions")
+    id("maven-publish")
 }
 
 android {
@@ -15,6 +16,19 @@ android {
         versionName = "0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+
+    sourceSets["main"].apply {
+        java.srcDirs("src/main/java", "src/main/kotlin")
+    }
+    sourceSets["test"].apply {
+        java.srcDirs("src/test/kotlin")
+    }
+    sourceSets["androidTest"].apply {
+        java.srcDirs("src/androidTest/kotlin")
+    }
+
+
 
     compileOptions {
         coreLibraryDesugaringEnabled = false
@@ -35,9 +49,38 @@ android {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$extra.kotlin_version")
-    implementation( "androidx.core:core-ktx:1.1.0")
-    implementation( "androidx.appcompat:appcompat:1.1.0")
-    testImplementation( "junit:junit:4.12")
-    androidTestImplementation( "androidx.test.ext:junit:1.1.1")
-    androidTestImplementation( "androidx.test.espresso:espresso-core:3.2.0")
+    implementation("androidx.core:core-ktx:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
+
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/aps/publish-test")
+                credentials {
+                    username =
+                        project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.github.aps"
+                artifactId = "publish-test"
+                version = "0.1"
+                from(components["release"])
+            }
+        }
+    }
+}
+
+val androidSourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets.getByName("main").java.srcDirs)
 }
