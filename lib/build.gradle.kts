0ebe1d76a03d433885af01d3a5dd0337070cafb9
Arne Poths <mail@arnepoths.de>
2020-08-20 16:35:37 +0200

    bump version

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdkVersion(21)
         targetSdkVersion(29)
 
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -73,7 +73,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.github.aps"
                 artifactId = "publish-test"
-                version = "0.1"
+                version = "0.2"
                 from(components["release"])
             }
         }
